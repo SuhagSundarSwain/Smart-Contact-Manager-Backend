895f6041b33a7fac9ffa216439bfbd014527e501
Suhag <suhagsundarswain1@gmail.com>
2024-10-19 18:29:04 +0530

    v1.8.2-SecurityConfig file comments removed to see the comments goto v1.8.1-cid: 99a92c3

--- src/main/java/com/SCM/Config/SecurityConfig.java
+++ src/main/java/com/SCM/Config/SecurityConfig.java
@@ -33,31 +33,6 @@ public class SecurityConfig {
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
-    /**
-     * // FOR IN MEMORY
-     * 
-     * @Bean
-     *       public UserDetailsService userDetailsService() {
-     *       UserDetails user1 =
-     *       User.withUsername("suhag").password("{noop}suhag").roles("ADMIN",
-     *       "USER").build();
-     *       UserDetails user2 =
-     *       User.withUsername("papun").password("{noop}papun").roles("USER").build();
-     *       UserDetails user3 =
-     *       User.withUsername("happy").password("{noop}happy").roles("ADMIN").build();
-     * 
-     *       InMemoryUserDetailsManager inMemoryUserDetailsManager = new
-     *       InMemoryUserDetailsManager(user1, user2, user3);
-     *       return inMemoryUserDetailsManager;
-     *       }
-     */
-
-    /**
-     * Configures the authentication provider to use custom user details service and
-     * bcrypt password encoder.
-     * 
-     * @return an instance of AuthenticationProvider
-     */
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
@@ -66,14 +41,6 @@ public class SecurityConfig {
         return daoAuthenticationProvider;
     }
 
-    /**
-     * Configures HTTP security settings, including URL authorization, CSRF, CORS,
-     * and disabling the default form login.
-     * 
-     * @param httpSecurity instance of HttpSecurity
-     * @return SecurityFilterChain for the application
-     * @throws Exception in case of configuration issues
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
@@ -88,75 +55,20 @@ public class SecurityConfig {
                 .formLogin(formLogin -> formLogin.disable())
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
-        /**
-         * httpSecurity.authorizeHttpRequests(authorize -> {
-         * authorize.requestMatchers("/user/**").authenticated();
-         * authorize.anyRequest().permitAll();
-         * });
-         * httpSecurity.csrf(csrf -> csrf.disable());
-         * httpSecurity.cors(Customizer.withDefaults());
-         * 
-         * httpSecurity.formLogin(formLogin -> {
-         * formLogin.loginPage("/login");
-         * formLogin.loginProcessingUrl("/login");
-         * formLogin.successForwardUrl("/user/dashboard");
-         * formLogin.failureForwardUrl("/login?error=true");
-         * formLogin.usernameParameter("email");
-         * formLogin.passwordParameter("password");
-         * 
-         * // formLogin.failureHandler(new AuthenticationFailureHandler() {
-         * // @Override
-         * // public void onAuthenticationFailure(HttpServletRequest request,
-         * // HttpServletResponse response,
-         * // AuthenticationException exception) throws IOException, ServletException {
-         * // // TODO Auto-generated method stub
-         * // throw new UnsupportedOperationException("Unimplemented method
-         * // 'onAuthenticationFailure'");
-         * // }
-         * // });
-         * // formLogin.successHandler(new AuthenticationSuccessHandler() {
-         * // @Override
-         * // public void onAuthenticationSuccess(HttpServletRequest request,
-         * // HttpServletResponse response,
-         * // Authentication authentication) throws IOException, ServletException {
-         * // // TODO Auto-generated method stub
-         * // throw new UnsupportedOperationException("Unimplemented method
-         * // 'onAuthenticationSuccess'");
-         * // }
-         * // });
-         * });
-         * return httpSecurity.build();
-         */
+
     }
 
-    /**
-     * Configures the password encoder to use BCrypt.
-     * 
-     * @return an instance of PasswordEncoder
-     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * Configures the authentication manager.
-     * 
-     * @param authenticationConfiguration the authentication configuration
-     * @return an instance of AuthenticationManager
-     * @throws Exception in case of configuration issues
-     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    /**
-     * Configures CORS settings, allowing requests from specific origins.
-     * 
-     * @return an instance of CorsConfigurationSource
-     */
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
